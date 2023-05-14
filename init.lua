df0d207443469aeaec5dec9d42bd9707095dae50
Ruslan.Nasyrov <goququ@ya.ru>
2023-05-14 18:28:47 +0500

    init

---
+++ init.lua
@@ -0,0 +1,5 @@
+local config = {
+	-- colorscheme = "tokyonight",
+}
+
+return config
---
+++ mappings.lua
@@ -0,0 +1,21 @@
+return {
+	n = {
+		["J"] = { "mzJ`z", desc = "" },
+		["n"] = { "nzzzv", desc = "Next match centered" },
+		["N"] = { "Nzzzv", desc = "Prev match cnetered" },
+	},
+	i = {
+		["<C-s>"] = { "<Esc><cmd>w!<cr>", desc = "Force write" },
+	},
+	v = {
+		["<C-s>"] = { "<Esc><cmd>w!<cr>", desc = "Force write" },
+		["J"] = { ":m '>+1<CR>gv=gv", desc = "Move line bottom" },
+		["K"] = { ":m '<-2<CR>gv=gv", desc = "Move line top" },
+		["<C-d>"] = { "<C-d>zz", desc = "" },
+		["<C-u>"] = { "<C-u>zz", desc = "" },
+		["p"] = { [["_dP]], desc = "Enhance paste" },
+	},
+	x = {
+		["<C-s>"] = { "<Esc><cmd>w!<cr>", desc = "Force write" },
+	},
+}
