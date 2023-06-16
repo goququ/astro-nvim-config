63a726a39b51438dfa4233fa3bbf32cae3f44961
Ruslan.Nasyrov <goququ@ya.ru>
2023-06-16 10:36:25 +0500

    added graphql lsp and treesitter

--- init.lua
+++ init.lua
@@ -4,6 +4,18 @@ local config = {
 		formatting = {
 			timeout_ms = 10000,
 		},
+		servers = {
+			"graphql",
+		},
+		config = {
+			graphql = function()
+				return {
+					cmd = { "graphql-lsp", "server", "-m", "stream" },
+					filetypes = { "graphql" },
+					root_dir = require("lspconfig").util.root_pattern(".git"),
+				}
+			end,
+		},
 	},
 	polish = function()
 		require("telescope").load_extension("projects")
