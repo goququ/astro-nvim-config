5a524b826f45251101fc20c7b656d932c6b71c3a
Ruslan Nasyrov <ruslan.nasyrov.f@gmail.com>
2024-03-03 23:22:21 +0500

    added custom mapping for live grep in folder for neotree

--- plugins/neotree.lua
+++ plugins/neotree.lua
@@ -11,6 +11,22 @@ return {
 			filtered_items = {
 				visible = true,
 			},
+			-- Add custom mappings here
+			window = {
+				mappings = {
+					["<C-f>"] = function(state)
+						-- Get the path of the currently selected item
+						local node = state.tree:get_node()
+						if node.type == "directory" then
+							-- If it's a directory, change Telescope's cwd to that directory and open find_files
+							require("telescope.builtin").live_grep({ cwd = node.path })
+						else
+							-- If it's not a directory, you can choose to do nothing, or customize this part
+							print("Not a directory")
+						end
+					end,
+				},
+			},
 		},
 	},
 }
